6bca3843b62d1641dbc41fc53939519ac64224a7
ImNotAOwl <siegel.matthieu@ymail.com>
2022-01-17 14:04:19 +0100

    add procedures backend

--- BackEnd/database/procedures/p_salles.sql
+++ BackEnd/database/procedures/p_salles.sql
@@ -9,7 +9,15 @@ BEGIN
 END //
 
 -- voir salle (carac)
+CREATE OR REPLACE PROCEDURE getOneSalle (IN p_nom VARCHAR(255))
+BEGIN
+	SELECT id, nom, description, capacite, prix FROM salles
+	WHERE nom = p_nom;
+END //
+
+
 	-- voir salle (liste resas dates)
+	
 -- créer salle
 -- update salle
 -- delete salle

1f44f64114f813562f913c70a5a1f87b96f95a46
Lem0nRavioli <hirona_mj@hotmail.fr>
2022-01-20 14:41:31 +0100

    routes/controllers/tests salles OK

--- BackEnd/routes/routes_salles.js
+++ BackEnd/routes/routes_salles.js
@@ -1,11 +1,35 @@
 const router = require('express-promise-router')();
 
 const {
-    getAllSalles
+    getAllSalles,
+    getActiveSalles,
+    getOneSalle,
+    getSalleResas,
+    createSalle,
+    updateSalle,
+    activeStatusSalle
 } = require('../controllers/controller_salles');
 
 router
-    .route('/')
+    .route('/all')
     .get(getAllSalles)
+
+router
+    .route('/')
+    .get(getActiveSalles)
+    .post(createSalle)
+
+router
+    .route('/:salle_id')
+    .get(getOneSalle)
+    .put(updateSalle)
+
+router
+    .route('/:salle_id/reservations')
+    .get(getSalleResas)
+
+router
+    .route('/:salle_id/activation')
+    .put(activeStatusSalle)
 
 module.exports = router;

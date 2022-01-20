1072ef1aec985700cfcb9d7f1d9fe87ac75ac475
BSevault <b.sevault@gmail.com>
2022-01-20 11:55:25 +0100

    added salles entries creation

--- BackEnd/database/schema.sql
+++ BackEnd/database/schema.sql
@@ -1,17 +1,17 @@
 -- M2L SQL DB CREATE SCRIPT --
 DROP DATABASE IF EXISTS M2L_DB;
+
 CREATE DATABASE M2L_DB;
 
 USE M2L_DB;
 
-
 CREATE TABLE USERS(
    id INT NOT NULL AUTO_INCREMENT,
-   nom VARCHAR(255)  NOT NULL,
-   prenom VARCHAR(255)  NOT NULL,
-   email VARCHAR(255)  NOT NULL,
+   nom VARCHAR(255) NOT NULL,
+   prenom VARCHAR(255) NOT NULL,
+   email VARCHAR(255) NOT NULL,
    tel VARCHAR(50),
-   password TEXT NOT NULL,
+   PASSWORD TEXT NOT NULL,
    ddn DATE NOT NULL,
    adresse VARCHAR(255),
    PRIMARY KEY(id)
@@ -19,7 +19,7 @@ CREATE TABLE USERS(
 
 CREATE TABLE SALLES(
    id INT NOT NULL AUTO_INCREMENT,
-   nom VARCHAR(255)  NOT NULL,
+   nom VARCHAR(255) NOT NULL,
    description VARCHAR(255),
    capacite INT NOT NULL,
    prix FLOAT NOT NULL,
@@ -29,7 +29,7 @@ CREATE TABLE SALLES(
 
 CREATE TABLE PRODUITS(
    id INT NOT NULL AUTO_INCREMENT,
-   nom_produit VARCHAR(255)  NOT NULL,
+   nom_produit VARCHAR(255) NOT NULL,
    description VARCHAR(255),
    qte_dispo INT NOT NULL,
    prix FLOAT NOT NULL,
@@ -40,7 +40,7 @@ CREATE TABLE PRODUITS(
 CREATE TABLE TICKETS(
    id INT NOT NULL AUTO_INCREMENT,
    date_ticket DATE NOT NULL,
-   date_probleme DATE NOT NULL DEFAULT "1850-01-01",
+   date_probleme DATE NOT NULL DEFAULT '1850-01-01',
    description TEXT NOT NULL,
    id_user INT NOT NULL,
    id_salle INT,
@@ -75,7 +75,6 @@ CREATE TABLE PAIEMENTS(
    FOREIGN KEY(id_reservation) REFERENCES RESERVATIONS(id)
 );
 
-
 CREATE TABLE PARTICIPANTS(
    id INT NOT NULL AUTO_INCREMENT,
    covid_positive BOOLEAN NOT NULL,
@@ -85,3 +84,16 @@ CREATE TABLE PARTICIPANTS(
    FOREIGN KEY(id_user) REFERENCES USERS(id),
    FOREIGN KEY(id_reservation) REFERENCES RESERVATIONS(id)
 );
+
+INSERT INTO SALLES (nom, description, capacite, prix)
+VALUES 
+('Majorelle', 'Service de sonorisation et vidéo projecteur disponible', 30, 2000),
+('Restauration et convivialité', 'Service de sonorisation et vidéo projecteur disponible', 50, 3000),
+('Grüber', 'Vidéo projecteur disponible', 20, 1000),
+('Lamour', 'Service de sonorisation et vidéo projecteur disponible', 25, 1500),
+('Amphithéâtre', 'Service de sonorisation et vidéo projecteur disponible', 100, 3500),
+('Longwy', 'Vidéo projecteur disponible', 15, 800),
+('Daum', 'Vidéo projecteur disponible', 20, 1000),
+('Gallé', 'Vidéo projecteur disponible', 20, 1000),
+('Corbin', 'Vidéo projecteur disponible', 20, 1000),
+('Baccarat', 'Vidéo projecteur disponible', 20, 1000);

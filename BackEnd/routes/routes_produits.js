881bb12676395354534d0b74052f84564d06c732
Lem0nRavioli <hirona_mj@hotmail.fr>
2022-01-20 15:58:07 +0100

    typo

--- BackEnd/routes/routes_produits.js
+++ BackEnd/routes/routes_produits.js
@@ -1,14 +1,6 @@
 const router = require('express-promise-router')();
 
 const {
-<<<<<<< HEAD
-    getAllSalles
-} = require('../controllers/controller_salles');
-
-router
-    .route('/')
-    .get(getAllSalles)
-=======
     getAllProducts,
     getProductById,
     createProduct,
@@ -29,6 +21,5 @@ router
     .put(updateProductById)
     .patch(updateProductQtyById)
     .delete(toggleProductIsActiveById)
->>>>>>> 2d88c21315e6f719c551b8df9906851c80cb3a9d
 
 module.exports = router;

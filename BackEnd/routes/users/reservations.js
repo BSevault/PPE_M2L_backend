371f495ff0131defe7eeb177ccaa822430cbac70
Lem0nRavioli <hirona_mj@hotmail.fr>
2022-01-24 11:20:45 +0100

    crud reservation OK

--- BackEnd/routes/users/reservations.js
+++ BackEnd/routes/users/reservations.js
@@ -1,11 +1,20 @@
 const router = require('express-promise-router')();
 
 const {
-    getReservations
+    getReservations, 
+    createReservation,
+    updateReservation,
+    toggleReservationIsPaid,
+    deleteReservation
 } = require('../../controllers/users/reservations');
 
 router
-    .route('/:user_id/reservations')
+    .route('/:user_id/reservation')
     .get(getReservations)
+    .post(createReservation)
+    .put(updateReservation)
+    .patch(toggleReservationIsPaid)
+    .delete(deleteReservation)
+    
 
 module.exports = router;

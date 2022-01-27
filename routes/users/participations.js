67f4567b99d95077eae0cf64df14946f433f1788
BSevault <b.sevault@gmail.com>
2022-01-27 17:50:02 +0100

    completed participations routes

--- routes/users/participations.js
+++ routes/users/participations.js
@@ -2,15 +2,26 @@ const router = require('express-promise-router')();
 
 const {
     createParticipant, 
-    getParticipationsByUserId
+    getParticipations,
+    getUserParticipationBefore, 
+    getUserParticipationAfter, 
+    updateParticipantCovidState, 
+    deleteParticipation
 } = require('../../controllers/users/participations');
 
 router
-    .route('/:user_id/:resa_id')
+    .route('/:user_id/participations')
     .post(createParticipant)
+    .get(getParticipations)
+    .put(updateParticipantCovidState)
+    .delete(deleteParticipation)
 
 router
-    .route('/:user_id/participations')
-    .get(getParticipationsByUserId)
+    .route('/:user_id/participations/before')
+    .get(getUserParticipationBefore)
+
+router
+    .route('/:user_id/participations/after')
+    .get(getUserParticipationAfter)
 
 module.exports = router;

5c17d1691be98f9197732d231a491cb9978b3771
Guillaume Saulnier <hirona_mj@hotmail.fr>
2022-03-11 09:18:09 +0100

    email proof ajout participants

--- controllers/users/participations.js
+++ controllers/users/participations.js
@@ -5,15 +5,20 @@ module.exports = {
     createParticipant: async (req, res) => {
         const { resa_id, email } = req.body;
         await call(res, async (connexion) => {
-            const result = await connexion.query("CALL createParticipation(?,?)", [email, resa_id] )
+
+            const checkEmail = await connexion.query("CALL getAccountIdByEmail(?)", [email]);
+            if (checkEmail[0][0] === undefined) {
+                return res.status(401).json({ error: "Un compte est déjà ouvert avec cette email" });
+            }
+            const result = await connexion.query("CALL createParticipation(?,?)", [email, resa_id])
             return res.status(200).json({ success: result });
         });
-    }, 
+    },
 
     getParticipations: async (req, res) => {
         const { user_id } = req.params;
         await call(res, async (connexion) => {
-            const result = await connexion.query("CALL getUserParticipations(?)", user_id )
+            const result = await connexion.query("CALL getUserParticipations(?)", user_id)
             if (result[0].length < 1) {
                 return res.status(300).json({ error: "Aucune participation" });
             } else {
@@ -25,19 +30,19 @@ module.exports = {
     getUserParticipationBefore: async (req, res) => {
         const { user_id } = req.params;
         await call(res, async (connexion) => {
-            const result = await connexion.query("CALL getUserParticipationBefore(?)", user_id )
+            const result = await connexion.query("CALL getUserParticipationBefore(?)", user_id)
             if (result[0].length < 1) {
                 return res.status(300).json({ error: "Aucune participation" });
             } else {
                 return res.status(200).json({ success: result[0] });
             }
         });
-    }, 
+    },
 
     getUserParticipationAfter: async (req, res) => {
         const { user_id } = req.params;
         await call(res, async (connexion) => {
-            const result = await connexion.query("CALL getUserParticipationAfter(?)", user_id )
+            const result = await connexion.query("CALL getUserParticipationAfter(?)", user_id)
             if (result[0].length < 1) {
                 return res.status(300).json({ error: "Aucune participation" });
             } else {
@@ -50,7 +55,7 @@ module.exports = {
         const { user_id } = req.params;
         const { date_positive } = req.body
         await call(res, async (connexion) => {
-            const result = await connexion.query("CALL isCovid(?,?)", [user_id, date_positive] )
+            const result = await connexion.query("CALL isCovid(?,?)", [user_id, date_positive])
             return res.status(200).json({ success: result });
         });
     },
@@ -59,9 +64,9 @@ module.exports = {
         const { user_id } = req.params;
         const { id_resa } = req.body
         await call(res, async (connexion) => {
-            const result = await connexion.query("CALL deleteParticipation(?,?)", [user_id, id_resa] )
+            const result = await connexion.query("CALL deleteParticipation(?,?)", [user_id, id_resa])
             return res.status(200).json({ success: result });
         });
     }
-    
+
 }

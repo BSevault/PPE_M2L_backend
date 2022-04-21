144278d11758deadc19fa09577a2abc7752bbacf
Guillaume Saulnier <hirona_mj@hotmail.fr>
2022-04-21 14:59:20 +0200

    refacto

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(express.json());
 app.use(cors(
     {
-        // credentials: true,
+        credentials: true,
         origin:"http://localhost:3000"
     }
 ));

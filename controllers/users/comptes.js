f25941f8ec218ce75b890593217349b84a96427c
Guillaume Saulnier <hirona_mj@hotmail.fr>
2022-04-21 14:44:10 +0200

    a

--- controllers/users/comptes.js
+++ controllers/users/comptes.js
@@ -38,7 +38,8 @@ module.exports = {
                     const user = await connexion.query("CALL getOneAccount(?)", [id_user]);
                     if (user[0][0].is_active) {
                         req.session.logged_user = user[0][0];
-                        // console.log(req?.session);
+                        console.log(req?.session);
+                        console.log(user[0]);
                         // console.log(req.session.logged_user);
                         return res.status(200).json({ success: user[0][0] });
                     }

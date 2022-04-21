360f958c3e41ec807877d60bb93bc5ee2eb214da
Guillaume Saulnier <hirona_mj@hotmail.fr>
2022-04-21 14:18:14 +0200

    update cors

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(cors(
     {
         credentials: true,
-        origin: "*" //"http://localhost:3000"
+        origin: "http://localhost:3000"
     }
 ));
 app.use(cookieParser());

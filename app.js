34144b86f6c9a2ed7940a58013c04b85e6bf1047
BSevault <b.sevault@gmail.com>
2022-04-25 14:11:07 +0200

    update app.js : added ip origin

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(cors(
     {
         credentials: true,
-        origin: ["http://localhost:3000", "http://192.168.0.61:3000"]
+        origin: ["http://localhost:3000", "http://192.168.0.61:3000", "http://192.168.56.1:3000"]
     }
 ));
 app.use(cookieParser());

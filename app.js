ce64060d88ab2be893382dffdf7df26035354616
Lem0nRavioli <hirona_mj@hotmail.fr>
2022-04-30 17:42:42 +0200

    typo

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.use(cors(
 
 app.use(session({
     secret: 'ma_session_super_secret_key',
-
+    // store ?
     saveUninitialized: false,
     resave: false,
     cookie: { path: '/', httpOnly: true, maxAge: 1000 * 60 * 60 * 24, secure: true},

ab6a4efbbbfbad17aa402cc9892f6a818ee134f9
Guillaume Saulnier <hirona_mj@hotmail.fr>
2022-05-05 10:21:29 +0200

    update gefor user /spit

--- database/permissions.sql
+++ database/permissions.sql
@@ -7,3 +7,8 @@ CREATE OR REPLACE USER 'ubuntu'@'localhost' IDENTIFIED BY 'Zod€7c@arT48';
 GRANT EXECUTE ON M2L_DB.* TO 'ubuntu'@'localhost';
 GRANT ALL ON M2L_DB.session TO 'ubuntu'@'localhost';
 GRANT ALL ON M2L_DB.sessions TO 'ubuntu'@'localhost';
+
+CREATE OR REPLACE USER 'gefor'@'localhost' IDENTIFIED BY 'Zod€7c@arT48';
+GRANT EXECUTE ON M2L_DB.* TO 'ubuntu'@'localhost';
+GRANT ALL ON M2L_DB.session TO 'ubuntu'@'localhost';
+GRANT ALL ON M2L_DB.sessions TO 'ubuntu'@'localhost';
